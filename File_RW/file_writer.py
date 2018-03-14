f78b1a741d8c408861883d58110d4845a074976a
Mark Johnson <mfjohnson98@yahoo.com>
2018-03-14 11:31:57 -0400

    Update hdfs endpoints

--- File_RW/file_writer.py
+++ File_RW/file_writer.py
@@ -5,4 +5,4 @@ data = [('First', 1), ('Second', 2), ('Third', 3), ('Fourth', 4), ('Fifth', 5)]
 df = sparkSession.createDataFrame(data)
 
 # Write into HDFS
-df.write.csv("http://api.hdfs.marathon.l4lb.thisdcos.directory/v1/endpoints/example.csv")
+df.write.csv("example.csv")
